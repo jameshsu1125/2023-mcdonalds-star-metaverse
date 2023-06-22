25e6f31587b8b5d606f27fe9bbf9e7a8bb9252df
jameshsu1125 <jameshsu1125@gmail.com>
2023-06-22 23:25:39 +0800

    optimize js

--- src/pages/question/frame/masters/index.js
+++ src/pages/question/frame/masters/index.js
@@ -1,23 +1,20 @@
-import { memo, useEffect } from 'react';
-import './index.less';
 import { Bezier, TweenProvider } from 'lesca-use-tween';
+import { memo } from 'react';
+import './index.less';
 
-const Masters = memo(({ prev }) => {
-	useEffect(() => {}, []);
-	return (
-		<div className='Masters'>
-			{[...new Array(12).keys()].map((e, i) => (
-				<TweenProvider
-					key={e}
-					defaultStyle={{ y: 200 }}
-					tweenStyle={{ y: 0 }}
-					options={{ delay: 200 + i * 10, duration: 500, easing: Bezier.easeOutBack }}
-				>
-					<div />
-				</TweenProvider>
-			))}
-			<div className='buttonContainer pointer-events-auto'>{prev}</div>
-		</div>
-	);
-});
+const Masters = memo(({ prev }) => (
+	<div className='Masters'>
+		{[...new Array(12).keys()].map((e, i) => (
+			<TweenProvider
+				key={e}
+				defaultStyle={{ y: 200 }}
+				tweenStyle={{ y: 0 }}
+				options={{ delay: 200 + i * 10, duration: 500, easing: Bezier.easeOutBack }}
+			>
+				<div />
+			</TweenProvider>
+		))}
+		<div className='buttonContainer pointer-events-auto'>{prev}</div>
+	</div>
+));
 export default Masters;
--- src/pages/result/userContent/userName/index.js
+++ src/pages/result/userContent/userName/index.js
@@ -1,13 +1,10 @@
-import { memo, useEffect } from 'react';
+import { memo } from 'react';
 import './index.less';
 
-const UserName = memo(({ userName }) => {
-	useEffect(() => {}, []);
-	return (
-		<div className='UserName'>
-			在另一個宇宙的
-			<span className='m-2 text-3xl'>{userName === '' ? '你' : userName}</span>
-		</div>
-	);
-});
+const UserName = memo(({ userName }) => (
+	<div className='UserName'>
+		在另一個宇宙的
+		<span className='m-2 text-3xl'>{userName === '' ? '你' : userName}</span>
+	</div>
+));
 export default UserName;
