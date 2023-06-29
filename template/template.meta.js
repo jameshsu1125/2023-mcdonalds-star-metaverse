c8a501f00e9d3c05156a98ff3ae1f435ec26eb58
jameshsu1125 <jameshsu1125@gmail.com>
2023-06-29 13:20:58 +0800

    Update template.meta.js

--- template/template.meta.js
+++ template/template.meta.js
@@ -2,6 +2,6 @@ module.exports = {
 	title: '你是哪種星級咖？ | 麥當勞1+1星級點',
 	description:
 		'從日常生活的選擇，測出你的專屬性格！是自帶明星級氣場的「C位娛樂星」麥脆鷄腿，還是軟硬實力兼具的「職場星天王」麥克雙牛堡？快測出專屬星級個性，找出與你最搭的1+1星級點朋朋！',
-	url: 'https://campaign.mcdonalds.com.tw/20231plus1quiz',
+	url: 'https://campaign.mcdonalds.com.tw/20231plus1quiz/',
 	// url: 'https://jameshsu1125.github.io/2023-mcdonalds-star-metaverse/',
 };
